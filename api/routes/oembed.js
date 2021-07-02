8720e50c1287f940e7a4eb2b083dc040675e8f61
LibusTech <68366979+LibusTech@users.noreply.github.com>
2021-07-02 22:06:51 +1000

    work maybe?

--- api/routes/oembed.js
+++ api/routes/oembed.js
@@ -7,10 +7,13 @@ router.get("/oembed", (req, res) => {
     title: "POG SCREENSHOT",
     url: req.query.url,
     thumbnail_url: req.query.url,
+    thumbnail_width: 1980,
+    thumbnail_height: 1080,
     width: 1980,
     length: 1080,
     provider_name: "xboxs.one",
     provider_url: "https://i.xboxs.one",
+    html: "<img src=" + req.query.url + ">",
   });
 });
 
