6f67059caca3b0f1367c3a1a12a1f61bbb2fb2d2
LibusTech <68366979+LibusTech@users.noreply.github.com>
2021-07-03 20:51:49 +1000

    fix path

--- api/routes/short.js
+++ api/routes/short.js
@@ -8,7 +8,7 @@ router.get("/short", (req, res) => {
     if (req.query.url) {
       randomString = nanoid(5);
 
-      fs.mkdir("../../../s.xboxs.one/" + randomString + "/", function (err) {
+      fs.mkdir("/s.xboxs.one/" + randomString + "/", function (err) {
         if (err) {
           console.log("failed to create directory", err);
           res.send("failed to create directory" + err);
