241365c1b0bc7dc531534b37c43acae1869ab22b
LibusTech <68366979+LibusTech@users.noreply.github.com>
2021-07-03 18:31:58 +1000

    fixes

--- api/routes/short.js
+++ api/routes/short.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const router = express.Router();
-const { nanoid } = require("nanoid");
-const fs = require("fs");
+// const express = require("express");
+// const router = express.Router();
+// const { nanoid } = require("nanoid");
+// const fs = require("fs");
 
-router.get("/short", (req, res) => {
-  if (req.query.key == "TheKey") {
-    if (req.query.url) {
-      randomString = nanoid(5);
+// router.get("/short", (req, res) => {
+//   if (req.query.key == "TheKey") {
+//     if (req.query.url) {
+//       randomString = nanoid(5);
 
-      fs.mkdir(randomString + "/", function (err) {
-        if (err) {
-          console.log("failed to create directory", err);
-        }
-      });
-    } else {
-      res.send("Please enter the long url");
-    }
-  } else {
-    res.send("Please enter the correct key");
-  }
+//       fs.mkdir(randomString + "/", function (err) {
+//         if (err) {
+//           console.log("failed to create directory", err);
+//         }
+//       });
+//     } else {
+//       res.send("Please enter the long url");
+//     }
+//   } else {
+//     res.send("Please enter the correct key");
+//   }
 
-  //  res.json({});
-});
+//   //  res.json({});
+// });
 
-module.exports = router;
+// module.exports = router;
