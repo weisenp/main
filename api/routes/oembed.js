3e68398a0a4530f791867735d14ded268cb21ab9
LibusTech <68366979+LibusTech@users.noreply.github.com>
2021-07-03 12:32:17 +1000

    made oembed get img size

--- api/routes/oembed.js
+++ api/routes/oembed.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const requestImageSize = require("request-image-size");
 
 router.get("/oembed", (req, res) => {
+  size = requestImageSize(req.query.url);
+
   res.json({
     type: "photo",
     title: "POG SCREENSHOT",
     url: req.query.url,
     thumbnail_url: req.query.url,
-    thumbnail_width: 1980,
-    thumbnail_height: 1080,
-    width: 1980,
-    length: 1080,
+    thumbnail_width: size.width,
+    thumbnail_height: size.height,
+    width: size.width,
+    length: size.height,
     provider_name: "xboxs.one",
     provider_url: "https://i.xboxs.one",
     html: "<img src=" + req.query.url + ">",
