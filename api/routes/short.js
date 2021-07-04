c6591ef3b2d89f40de3fef88e8b88b14fb809f95
LibusTech <68366979+LibusTech@users.noreply.github.com>
2021-07-04 17:32:40 +1000

    finish short

--- api/routes/short.js
+++ api/routes/short.js
@@ -6,17 +6,27 @@ const fs = require("fs");
 router.get("/short", (req, res) => {
   if (req.query.key == "TheKey") {
     if (req.query.url) {
-      randomString = nanoid(5);
+      randomString = nanoid(3);
 
       fs.mkdir(
         "/home/xboxs.one/s.xboxs.one/" + randomString + "/",
         function (err) {
           if (err) {
-            console.log("failed to create directory", err);
-            res.send("failed to create directory" + err);
+            res.send("failed to create directory " + err);
           }
 
-          res.send("created");
+          let file = `<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8' /><meta http-equiv='X-UA-Compatible' content='IE=edge' /><meta name='viewport' content='width=device-width, initial-scale=1.0' /><meta http-equiv='refresh' content='0; url=${req.query.url}' /></head><body></body></html>`;
+          fs.writeFile(
+            "/home/xboxs.one/s.xboxs.one/index.html",
+            file,
+            function (err) {
+              if (err) {
+                res.send("failed to create index.js " + err);
+              }
+
+              res.send(`https:/s.xboxs.one/${randomString}`);
+            }
+          );
         }
       );
     } else {
